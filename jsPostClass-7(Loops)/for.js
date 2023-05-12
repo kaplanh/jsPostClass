0b51ee4abedcb74fb84e8b393ecf52edd741f43e
kplnhsyn <kplnhsyn.49@gmail.com>
2023-05-12 09:49:49 +0200

    asal sayi

--- jsPostClass-7(Loops)/for.js
+++ jsPostClass-7(Loops)/for.js
@@ -123,21 +123,22 @@ console.log(" **** FOR ****");
 //* 1 ve kendisinden başka böleni olmayan sayılar ASAL sayılardır.
 //* Eğer girilen sayı, herhangi bir sayıya tam bölünüyorsa diğer sayıları kontrol etmeye gerek yoktur. ASAL DEĞİL diyebiliriz.
 //* break deyimi ile dongu bir koşul gerçekleştiğinde kırılabilir.
-
 // ?1.yol
-// let sayi = +prompt("Enter a number");
 
-// if (sayi === 2) {
+// let sayi = prompt("Enter a number");
+
+// if (sayi == 2) {
 //   console.log(`${sayi} asaldir`);
-// } else if (sayi === 1) {
+// } else if (sayi == 1) {
 //   console.log(`${sayi} asal degildir`);
 // } else {
 //   for (let i = 2; i < sayi; i++) {
-//     if (sayi % i === 0) {
+//     if (sayi % i == 0) {
 //       console.log(`${sayi} asal degildir`);
 //       break;
 //     } else {
 //       console.log(`${sayi} asaldir`);
+//       break;
 //     }
 //   }
 // }
@@ -177,3 +178,16 @@ console.log(" **** FOR ****");
 // }
 
 // console.log(flag ? "Asal" : "Asal degil");
+
+// ?4.yol
+// let isPrime = +prompt("Please enter the number");
+// let count = 0;
+// for (let i = 2; i < isPrime; i++) {
+//   if (isPrime % i == 0) {
+//     count += 1;
+//     break;
+//   }
+// }
+// console.log(
+//   isPrime <= 1 ? "Hic Asal değil" : count > 0 ? "Asal degil" : "Asal"
+// );
--- jsPostClass-7(Loops)/while.js
+++ jsPostClass-7(Loops)/while.js
@@ -4,12 +4,12 @@
 
 console.log("****** WHILE *******");
 // ##
-let i = 1;
-while (i <= 10) {
-  console.log(i);
-  i++;
-}
-console.log("BYE");
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+// console.log("BYE");
 
 // ?10 kere konsola Merhaba yazdıran uygulamayı while döngüsü ile
 // yazınız
@@ -23,3 +23,54 @@ console.log("BYE");
 //! ------  DO - WHILE -------
 
 //! ------  FOR -------
+
+
+
+
+
+// ========================================================================================================
+
+// let answer;
+// let i=0;
+// let computer=Math.round(Math.random()*100);
+// while (true){
+//     while (i<7){
+//         let user=+prompt("Please enter  a number between 0-100");
+//         if (computer===user){
+//             console.log("Mastermind")
+//             break
+//         } else if (user<computer){
+//             console.log("Your number is lower than computer, increase your number")
+//             i++
+//             continue
+//         } else {
+//             console.log("Your number is greater than computer, decrease your number")
+//             i++
+//             continue
+//         }
+//     }
+//     if (i<7) {
+//         console.log("You have found secret number ")
+//         answer=prompt("Do you wanna play agein 'Y' or 'N'").toUpperCase()
+//         if (answer==="Y") {
+//             console.log("Great")
+//             computer=Math.round(Math.random()*100);
+//             i=0;
+//             continue
+//         } else {
+//             console.log("Have a nice day")
+//             break
+//         }
+//     }else {
+//             console.log(` You have lost . computer's number ${computer}`)
+//             answer=prompt("Do you wanna play agein'Y' or 'N' ").toUpperCase()
+//         if (answer==="Y") {
+//             console.log("Great")
+//             computer=Math.round(Math.random()*100);
+//             i=0;
+//             continue
+//         } else {
+//             console.log("Have a nice day")
+//             break
+//         }
+//     }
