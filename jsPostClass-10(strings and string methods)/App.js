765106fa06213e922ff9b7a0764e4206b80067b5
kplnhsyn <kplnhsyn.49@gmail.com>
2023-05-18 11:23:36 +0200

    .concat(),upperCase(),trim(),split(),slice()

--- jsPostClass-10(strings and string methods)/App.js
+++ jsPostClass-10(strings and string methods)/App.js
@@ -133,10 +133,119 @@
 // console.log(str2);
 
 // *\n ' örnek
-alert("1- ders \n2-Kayit\n3-pre-cla\bss \t tab örnegi");
-console.log("1- ders \n2-Kayit\n3-pre-cla\b ss \t tab örnegi");
+// alert("1- ders \n2-Kayit\n3-pre-class");
+
 // *soru:bunu baska nasil yapabilirdik
 
 // alert(`1- ders
 // 2-Kayıt
 // 3-pre-class`)
+
+// *\t örnek
+// console.log("1- ders\ttab örnegi");
+
+// *\b örnek
+// alert("1- d\bers ");
+// !JavaScript String() Function
+// const a = 225; // number
+// const b = true; // boolean
+
+// //converting to string
+// const result1 = String(a);
+// const result2 = String(b);
+
+// console.log(result1); // "225"
+// console.log(result2); // "true"
+
+// !String Metodlari
+// * Örnekler
+// *1// concatenating two strings
+// const text1 = "hello";
+// const text2 = "  world";
+// const text3 = "     JavaScript     ";
+
+// const result1 = text1.concat(" ", text2);
+// console.log(result1);
+
+// const result2 = text1.concat(" ", text2, " ", text3);
+// console.log(result2);
+
+// *2converting the text to uppercase
+// const text1 = "hello world,js html";
+// const text2 = "  world";
+
+// const result2 = text1.toUpperCase();
+// console.log(result2);
+
+// const result4 = text2.toUpperCase();
+// console.log(result4);
+
+// *3// removing whitespace from the string
+// const text1 = "hello world,js html";
+// const text2 = "  world";
+// const text3 = "     JavaScript     ";
+// const text4 = "html,css,JavaScript,react"
+
+// const result3 = text3.trim();
+// console.log(result3);
+
+// const result5 = text2.trimStart();
+// const result6 = text2.trimEnd();
+// console.log(result5);
+// console.log(result6);
+
+// * 4 converting the string to an array
+// const text1 = "hello world,js html";
+// const text2 = "  world";
+// const text3 = "     JavaScript     ";
+// const text4 = "html,css,JavaScript,react";
+
+// const result4 = text1.split();
+// const result5 = text1.split("");
+// const result6 = text1.split(" ");
+// const result7 = text1.split(" ", 1);
+// const result8 = text1.split(" ", 4);
+// const result9 = text1.split(",");
+// console.log(result4);
+// console.log(result5);
+// console.log(result6);
+// console.log(result7);
+// console.log(result8);
+// console.log(result9);
+
+// const result10 = text4.split(",", 3);
+// console.log(result10);
+
+// *5// slicing the string
+// const text1 = "hello world,js html";
+// const text2 = "  world";
+// const text3 = "     JavaScript     ";
+// const text4 = "html,css,JavaScript,react";
+
+// const result5 = text1.slice();
+// const result6 = text1.slice(4);
+// const result7 = text1.slice(0, 3);
+// const result8 = text1.slice(-1);
+// const result9 = text1.length;
+// const result10 = text1.slice(20);
+// const result11 = text1.slice(3, 0);
+// const result12 = text1.slice(-19, 19);
+// const result121 = text1.slice(-19, text1.length);
+// const result13 = text1.slice(-18, -1);
+// console.log(result5);
+// console.log(result6);
+// console.log(result7);
+// console.log(result8);
+// console.log(result9);
+// console.log(result10);
+// console.log(result11);
+// console.log(result12);
+// console.log(result121);
+// console.log(result13);
+
+// const result8 = text1.slice(text1.length - 1);
+// const result9 = text1.slice(-1);
+
+// console.log(result8);
+// console.log(result9);
+// console.log(result10);
