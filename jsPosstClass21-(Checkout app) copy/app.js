c932b6ee61fd9d6f7aa525fe2264bd502292b050
kplnhsyn <kplnhsyn.49@gmail.com>
2023-06-13 13:55:59 +0200

    completed

--- jsPosstClass21-(Checkout app) copy/app.js
+++ jsPosstClass21-(Checkout app) copy/app.js
@@ -14,48 +14,68 @@ console.log(ürünlerPanelDivi);
       if (confirm("Silmek istediğinizden emin misiniz?")) {
         e.target.closest(".main__product").remove();
         araToplam();
-        
       }
     }
   } else if (e.target.classList.contains("fa-plus")) {
     e.target.previousElementSibling.textContent++;
-    ürünCarpiBirimFiyat(e.target.closest(".main__product-info")); 
+    ürünCarpiBirimFiyat(e.target.closest(".main__product-info"));
   } else if (e.target.classList.contains("fa-trash-can")) {
     if (confirm("Silmek istediğinizden emin misiniz?")) {
       // e.target.closest(".main__product").remove();
       e.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.remove();
       araToplam();
-      
     }
   }
 });
 
-
 const ürünCarpiBirimFiyat = (ürünPaneli) => {
   // let miktar = parseFloat(document.getElementById("quantity").textContent);
   // console.log(miktar);
-  let miktar = ürünPaneli.querySelector('#quantity').innerText;
+  let miktar = ürünPaneli.querySelector("#quantity").innerText;
   console.log(miktar);
-  let fiyat = ürünPaneli.querySelector('strong').innerText;
-  console.log(fiyat);
-  let miktarCarpiFiyat = ürünPaneli.querySelector(
-    ".main__product-line-price");
+  let fiyat = ürünPaneli.querySelector("strong").innerText;
+  // console.log(fiyat);
+  let miktarCarpiFiyat = ürünPaneli.querySelector(".main__product-line-price");
   miktarCarpiFiyat.innerText = (miktar * fiyat).toFixed(2);
   console.log(miktarCarpiFiyat);
-  araToplam()
-}
+  araToplam();
+};
 
 const araToplam = () => {
   const miktarCarpiFiyatlarListesi = document.querySelectorAll(
     ".main__product-line-price"
-  );//NodeList döndürür
-  let subTotal = 0
+  ); //NodeList döndürür
+  let subTotal = 0;
   miktarCarpiFiyatlarListesi.forEach((item) => {
     subTotal += parseFloat(item.innerText);
   });
   const araToplamFiyat = document.querySelector(".main__sum-price");
   araToplamFiyat.innerText = subTotal.toFixed(2);
-}
 
+  const shipping = document.querySelector(".card-shipping");
+  if (araToplamFiyat.innerText > 3000 || araToplamFiyat.innerText <= 0) {
+    shipping.innerText = 0;
+  } else {
+    shipping.innerText = 25.99;
+  }
 
+  const tax = document.querySelector(".card-tax");
+  tax.innerText = (araToplamFiyat.innerText * vergiOrani).toFixed(2);
 
+  const genelToplam = document.querySelector(".total");
+  genelToplam.innerText = (
+    parseFloat(araToplamFiyat.innerText) +
+    parseFloat(shipping.innerText) +
+    parseFloat(tax.innerText)
+  ).toFixed(2);
+
+  // if (subTotal <= 0 || subTotal >= 3000) {
+  //   kargo = 0;
+  // } else {
+  //   kargo = 25;
+  // }
+  // const vergi = subTotal * vergiOrani;
+  // console.log(vergi);
+  // const genelToplam = subTotal + vergi + kargo;
+  // console.log(genelToplam);
+};
